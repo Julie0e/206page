f7f464f5e17fcafa4ce26f5ec445153dad309391
Julie0e <julie_91@hanmail.net>
2014-01-16 13:52:45 +0900

    206page 완성했습니다.

--- 206page/ViewController.h
+++ 206page/ViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate,
+AVAudioPlayerDelegate, AVAudioRecorderDelegate>
+{
+}
 
 @end
